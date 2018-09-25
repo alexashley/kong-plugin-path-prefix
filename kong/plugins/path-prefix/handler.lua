4de3fb66207f23b02d322dd6c34750e069db2c70
Alex Ashley <alexashley15@gmail.com>
2018-09-24 22:10:29 -0500

    Allow for the path to replace to include hyphens

--- kong/plugins/path-prefix/handler.lua
+++ kong/plugins/path-prefix/handler.lua
@@ -4,11 +4,26 @@ function plugin:new()
     plugin.super.new(self, "path-prefix")
 end
 
+local function escape_hyphen()
+    local conf = kong.ctx.plugin.conf
+    local path_prefix = conf.path_prefix
+    local should_escape = conf.escape
+
+    if should_escape then
+       return string.gsub(path_prefix, "%-", "%%%1")
+    end
+
+    return path_prefix
+end
+
 function plugin:access(plugin_conf)
     plugin.super.access(self)
 
+    kong.ctx.plugin.conf = plugin_conf
+
     local full_path = kong.request.get_path()
-    local path_without_prefix = full_path:gsub("%" .. plugin_conf.path_prefix, "")
+    local replace_match = escape_hyphen()
+    local path_without_prefix = full_path:gsub(replace_match, "", 1)
 
     if path_without_prefix == "" and plugin_conf.service_path == "" then
         path_without_prefix = "/"
--- kong/plugins/path-prefix/schema.lua
+++ kong/plugins/path-prefix/schema.lua
@@ -2,6 +2,7 @@ return {
   no_consumer = true,
   fields = {
     path_prefix = {type = "string", required = true, },
-    service_path = {type = "string", required = true, default = ""}
+    service_path = {type = "string", required = true, default = ""},
+    escape = {type = "bool", default = true}
   },
 }
