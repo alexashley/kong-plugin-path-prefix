3ba5a6c4f27b1c1e7011528cc05909b2e5b20fcb
Ricardo M. Augusto <rmaugusto@gmail.com>
2020-07-07 23:25:45 -0300

    Update plugin schema for Kong 1.x (#3)

--- kong/plugins/path-prefix/schema.lua
+++ kong/plugins/path-prefix/schema.lua
@@ -1,7 +1,31 @@
+local typedefs = require "kong.db.schema.typedefs"
+
 return {
-  no_consumer = true,
+  name = "path-prefix",
   fields = {
-    path_prefix = {type = "string", required = true, },
-    escape = {type = "bool", default = true}
+    {
+      consumer = typedefs.no_consumer
+    },
+    {
+      run_on = typedefs.run_on_first
+    },
+    {
+      protocols = typedefs.protocols_http
+    },
+    {
+      config = {
+        type = "record",
+
+        fields = {
+
+          { path_prefix = {type = "string", required = true} },
+          { escape = {type = "boolean", default = true} },
+
+        }, 
+
+      },
+    },
+  },
+  entity_checks = {
   },
 }
